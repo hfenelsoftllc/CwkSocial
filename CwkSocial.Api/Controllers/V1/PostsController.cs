a6392982ad63713a7c353bc4815b21be3b0d0ec9
hfenelsoftllc <hfenelsoftllc@outlook.com>
2021-12-21 01:55:48 -0500

    Add project files.

---
+++ CwkSocial.Api/Controllers/V1/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using CwkSocial.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwkSocial.Api.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PostsController: Controller
+    {
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var post = new Post { Id = id, Text = "Hello, World" };
+            return Ok(post);
+        }
+
+    }
+}
---
+++ CwkSocial.Api/Controllers/V2/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using CwkSocial.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwkSocial.Api.Controllers.V2
+{
+    [ApiVersion("2.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class PostsController : Controller
+    {
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var post = new Post { Id = id, Text = "Hello, Api V2" };
+            return Ok(post);
+        }
+
+    }
+}
